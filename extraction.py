b2f5165ccfef1af036e5b0347398dbbbbb0370df
Akkshay <akkshaysr2@gmail.com>
2023-05-25 15:48:06 +0530

    Initial project + data

---
+++ extraction.py
@@ -0,0 +1,17 @@
+import camelot
+import pandas as pd
+
+
+def extract_pdf(path, outpath, pages):
+    tables = camelot.read_pdf(path, pages=f'1-{pages}')
+    all_frames = []
+    for i in range(len(tables)):
+        subtable = tables[i].df
+        subtable.drop([0, 1], inplace=True)
+        all_frames.append(subtable)
+    df = pd.concat(all_frames, ignore_index=False)
+    df.to_csv(outpath, index=False)
+
+
+if __name__ == "__main__":
+    extract_pdf("./data/pdf/2021_lake_data.pdf", "./data/csv/2021lake_data.csv", 32)
